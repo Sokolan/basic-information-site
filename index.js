85a2e23823dff5807b2914e6e902bf4aea9e7278
Sokolan <valerysokolan@gmail.com>
2024-05-01 18:35:17 +0300

    added statusCode

--- index.js
+++ index.js
@@ -3,23 +3,28 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-type", "text/html");
-  const reqUrl = req.url;
+
   let pageToLoad;
-  switch (reqUrl) {
+  switch (req.url) {
     case "/": {
       pageToLoad = "index";
+      res.statusCode = 200;
       break;
     }
     case "/about": {
       pageToLoad = "about";
+      res.statusCode = 200;
       break;
     }
     case "/contact-me": {
       pageToLoad = "contact-me";
+      res.statusCode = 200;
       break;
     }
     default: {
       pageToLoad = "404";
+      res.statusCode = 404;
+      break;
     }
   }
   fs.readFile(`./${pageToLoad}.html`, (err, data) => {
